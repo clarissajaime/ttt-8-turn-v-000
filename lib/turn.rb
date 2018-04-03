66fc420232dda1c9e47051b2ce9bf70f01238029
Clarissa Jaime <clarissanjaime@gmail.com>
2018-04-03 00:56:13 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !index.between?(0, 8)
+    return false
+  elsif position_taken?(board, index) == true
+    return false
+  elsif position_taken?(board, index) == false
+    return true
+  end
+end
+
